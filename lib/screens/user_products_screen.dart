53188c4f11ec51281862110ca365ae43fad21b3f
Jerry Rodriguez <jrvj01@hotmail.com>
2020-11-08 17:37:50 -0400

    268.1-269. completing attaching orders to users

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -39,19 +39,20 @@ class UserProductsScreen extends StatelessWidget {
                 : RefreshIndicator(
                     onRefresh: () => _refreshProducts(context),
                     child: Consumer<Products>(
-                      builder: (ctx, productsData, _) => 
-                      Padding(
+                      builder: (ctx, productsData, _) => Padding(
                         padding: const EdgeInsets.all(8),
                         child: ListView.builder(
                           itemCount: productsData.items.length,
-                          itemBuilder: (_, i) => Column(children: [
-                            UserProductItem(
-                              productsData.items[i].id,
-                              productsData.items[i].title,
-                              productsData.items[i].imageUrl,
-                            ),
-                            Divider()
-                          ]),
+                          itemBuilder: (_, i) => Column(
+                            children: [
+                              UserProductItem(
+                                productsData.items[i].id,
+                                productsData.items[i].title,
+                                productsData.items[i].imageUrl,
+                              ),
+                              Divider()
+                            ],
+                          ),
                         ),
                       ),
                     ),
